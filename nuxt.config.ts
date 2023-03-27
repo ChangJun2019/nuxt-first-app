e8587f6480bd902420d83aacfc0ec6ab2b1637d8
changjun <52chinaweb@gmail.com>
2023-03-27 17:16:56 +0800

    chore: add macros vueuse icon devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: [
+    '@vue-macros/nuxt',
+    '@vueuse/nuxt',
+    '@nuxtjs/color-mode',
+    'nuxt-icon',
+    '@nuxt/devtools',
+  ],
 
+  // https://vue-macros.sxzz.moe/guide/configurations.html
+  macros: {},
+
+  // https://color-mode.nuxtjs.org/#configuration
+  colorMode: {
+    preference: 'system',
+    dataValue: 'theme',
+    classSuffix: '',
+  },
+
+  // https://github.com/nuxt/devtools/blob/main/packages/devtools-kit/src/_types/module-options.ts
+  devtools: {
+    enabled: true,
+  },
 })
